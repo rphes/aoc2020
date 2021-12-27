2999ab820ef96d91134f2330456c631dd62ac12c
Robin Hes <robinhes@outlook.com>
2021-12-27 17:06:17 +0100

    Rewrote day 20 part 1.

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,13 +1,14 @@
 use std::collections::HashMap;
+use std::fmt;
 use std::fs;
 use std::iter::Iterator;
 use std::time::Instant;
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Could not read file.");
-    let (tiles, edges) = parse_input(&contents).expect("Could not parse tiles");
+    let tiles = parse_input(&contents).expect("Could not parse tiles");
     let p1 = Instant::now();
-    let neighbor_map = find_neighbors(&edges);
+    let neighbor_map = find_neighbors(&tiles);
 
     println!("Part 1: {} ({:?}).", part1(&neighbor_map), p1.elapsed());
     // let p2 = Instant::now();
@@ -21,21 +22,94 @@ fn main() {
 type Edges = [String; 4];
 type EdgeMap = HashMap<usize, Edges>;
 type TileMap = HashMap<usize, Vec<String>>;
+const TILE_SIZE: usize = 10;
 
-fn parse_input(contents: &str) -> Option<(TileMap, EdgeMap)> {
-    let data = contents
-        .split("\n\n")
-        .map(parse_tile)
-        .collect::<Option<Vec<_>>>()?;
+#[derive(PartialEq, Copy, Clone)]
+struct Edge(u16);
 
-    Some(
-        data.into_iter()
-            .map(|(id, data, edges)| ((id, data), (id, edges)))
-            .unzip(),
-    )
+impl fmt::Debug for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let text: String = (0..TILE_SIZE)
+            .map(|i| if self.0 & (1 << i) != 0 { '#' } else { '.' })
+            .collect();
+        write!(f, "{}", text)
+    }
 }
 
-fn parse_tile(string: &str) -> Option<(usize, Vec<String>, Edges)> {
+impl Edge {
+    pub fn reverse(&self) -> Self {
+        Self(self.0.reverse_bits().rotate_left(TILE_SIZE as u32))
+    }
+}
+
+struct Tile {
+    id: usize,
+    rows: Vec<Edge>,
+}
+
+impl Tile {
+    pub fn left_edge(&self) -> Edge {
+        return self.get_col(0);
+    }
+
+    pub fn right_edge(&self) -> Edge {
+        return self.get_col(TILE_SIZE - 1);
+    }
+
+    pub fn top_edge(&self) -> Edge {
+        self.rows[0]
+    }
+
+    pub fn bottom_edge(&self) -> Edge {
+        self.rows[TILE_SIZE - 1]
+    }
+
+    pub fn edges(&self) -> [Edge; 4] {
+        [
+            self.top_edge(),
+            self.right_edge(),
+            self.bottom_edge(),
+            self.left_edge(),
+        ]
+    }
+
+    pub fn get_row(&self, row_idx: usize) -> Edge {
+        self.rows[row_idx as usize]
+    }
+
+    pub fn get_col(&self, col_idx: usize) -> Edge {
+        return Edge(self.rows.iter().enumerate().fold(0, |col, (row_idx, row)| {
+            col | (row.0 & 1 << col_idx as u16) >> col_idx << row_idx
+        }));
+    }
+
+    pub fn rotate(&self) -> Self {
+        return Self {
+            id: self.id,
+            rows: (0..TILE_SIZE)
+                .map(|col| self.get_col(col).reverse())
+                .collect(),
+        };
+    }
+}
+
+impl fmt::Debug for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in &self.rows {
+            write!(f, "{:?}\n", row);
+        }
+        Ok(())
+    }
+}
+
+fn parse_input(contents: &str) -> Option<Vec<Tile>> {
+    contents
+        .split("\n\n")
+        .map(parse_tile)
+        .collect::<Option<Vec<_>>>()
+}
+
+fn parse_tile(string: &str) -> Option<Tile> {
     let (header, tile) = string.split_once('\n')?;
     let id: usize = header
         .strip_prefix("Tile ")?
@@ -44,48 +118,45 @@ fn parse_tile(string: &str) -> Option<(usize, Vec<String>, Edges)> {
         .ok()?;
 
     let lines: Vec<&str> = tile.lines().collect();
-    let top = lines.get(0)?.to_string();
-    let bottom = lines.last()?.chars().rev().collect::<String>();
-    let left = lines
-        .iter()
-        .rev()
-        .map(|x| x.chars().nth(0))
-        .collect::<Option<_>>()?;
-    let right = lines
-        .iter()
-        .map(|x| x.chars().last())
-        .collect::<Option<_>>()?;
 
-    return Some((
+    return Some(Tile {
         id,
-        lines.iter().map(|s| s.to_string()).collect(),
-        [top, right, bottom, left],
-    ));
+        rows: lines
+            .iter()
+            .map(|line| {
+                Edge(line.chars().enumerate().fold(
+                    0,
+                    |row, (i, c)| {
+                        if c == '#' {
+                            row | (1 << i)
+                        } else {
+                            row
+                        }
+                    },
+                ))
+            })
+            .collect(),
+    });
 }
 
-type Neighbors = HashMap<usize, [Option<(usize, usize, bool)>; 4]>;
+type Neighbors = HashMap<usize, Vec<usize>>;
 
-fn find_neighbors(edge_map: &EdgeMap) -> Neighbors {
-    let mut neighbor_map: Neighbors = edge_map.keys().map(|k| (*k, [None; 4])).collect();
+fn find_neighbors(tiles: &[Tile]) -> Neighbors {
+    let mut neighbor_map: Neighbors = tiles
+        .iter()
+        .map(|tile| (tile.id, Vec::with_capacity(4)))
+        .collect();
 
-    for (id, edges) in edge_map {
-        if neighbor_map[id].iter().filter(|e| e.is_some()).count() == 4 {
-            continue;
-        }
+    for tile in tiles {
+        for tile2 in tiles {
+            if tile.id == tile2.id {
+                continue;
+            }
 
-        for (id2, edges2) in edge_map {
-            if id != id2 {
-                for (idx, edge) in edges.iter().enumerate() {
-                    if neighbor_map[id][idx].is_some() {
-                        continue;
-                    }
-
-                    for (idx2, edge2) in edges2.iter().enumerate() {
-                        let reverse = edge == edge2;
-                        if edge == &edge2.chars().rev().collect::<String>() || reverse {
-                            neighbor_map.get_mut(id2).unwrap()[idx2] = Some((*id, idx, reverse));
-                            neighbor_map.get_mut(id).unwrap()[idx] = Some((*id2, idx2, reverse));
-                        }
+            for edge in tile.edges().iter() {
+                for edge2 in tile2.edges().iter() {
+                    if edge == edge2 || *edge == edge2.reverse() {
+                        neighbor_map.get_mut(&tile.id).unwrap().push(tile2.id)
                     }
                 }
             }
@@ -97,65 +168,8 @@ fn find_neighbors(edge_map: &EdgeMap) -> Neighbors {
 fn part1(neighbor_map: &Neighbors) -> usize {
     neighbor_map
         .iter()
-        .filter_map(|(k, v)| {
-            if v.iter().filter(|e| e.is_some()).count() == 2 {
-                Some(*k)
-            } else {
-                None
-            }
-        })
+        .filter_map(|(k, v)| if v.len() == 2 { Some(*k) } else { None })
         .product()
-}
-
-// fn transpose(tile: Vec<&str>) -> Vec<&str> {}
-
-// fn rotate(tile: Vec<&str>) -> Vec<&str> {
-//     vec![""]
-// }
-
-fn part2(neighbor_map: &Neighbors, tile_map: &TileMap) -> usize {
-    let mut edge = 2i8;
-    let mut mirror_x = false;
-    let mut mirror_y = false;
-    let mut neighbors = neighbor_map
-        .values()
-        .find(|v| v[0].is_none() && v[1].is_some() && v[2].is_some() && v[3].is_none())
-        .unwrap();
-
-    loop {
-        let mut neighbors2 = neighbors;
-        let mut edge2 = (edge - 1).rem_euclid(4);
-
-        while let Some(right_neighbor) = neighbors2[edge2 as usize] {
-            let (id, local_edge_num, local_reverse) = right_neighbor;
-
-            if local_reverse {
-                if local_edge_num % 2 == 0 {
-                    mirror_x = mirror_x ^ local_reverse;
-                } else {
-                    mirror_y = mirror_y ^ local_reverse;
-                }
-            }
-
-            edge2 = (edge2 + (3 - local_edge_num as i8)).rem_euclid(4);
-            neighbors2 = neighbor_map.get(&id).unwrap();
-            println!("{:?} {:?} {:?}", id, local_edge_num, edge2);
-        }
-        println!("");
-
-        if let Some(bottom_neighbor) = neighbors[edge as usize] {
-            let (id, local_edge_num, local_reverse) = bottom_neighbor;
-            edge = (edge + (0 - local_edge_num as i8)).rem_euclid(4);
-            neighbors = neighbor_map.get(&id).unwrap();
-            println!("{:?} {:?} {:?}", id, local_edge_num, edge);
-        } else {
-            break;
-        }
-    }
-
-    println!("{:?}", neighbors);
-
-    0
 }
 
 #[cfg(test)]
@@ -270,123 +284,116 @@ Tile 3079:
 ..#.......
 ..#.###...";
 
-    #[test]
-    fn test_parse_input() {
-        let edge_map = parse_input(TEST_INPUT).unwrap().1;
-        assert_eq!(
-            edge_map.get(&2311),
-            Some(&[
-                "..##.#..#.".to_string(),
-                "...#.##..#".to_string(),
-                "###..###..".to_string(),
-                ".#..#####.".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&1951),
-            Some(&[
-                "#.##...##.".to_string(),
-                ".#####..#.".to_string(),
-                "..#.##...#".to_string(),
-                "#..#..#.##".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&1171),
-            Some(&[
-                "####...##.".to_string(),
-                ".#..#.....".to_string(),
-                "...##.....".to_string(),
-                ".##....###".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&1427),
-            Some(&[
-                "###.##.#..".to_string(),
-                "..###.#.#.".to_string(),
-                ".#..#.##..".to_string(),
-                "......#..#".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&1489),
-            Some(&[
-                "##.#.#....".to_string(),
-                ".....#..#.".to_string(),
-                "..#.##.###".to_string(),
-                "#.#.##...#".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&2473),
-            Some(&[
-                "#....####.".to_string(),
-                "...###.#..".to_string(),
-                ".#.#.###..".to_string(),
-                ".##...####".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&2971),
-            Some(&[
-                "..#.#....#".to_string(),
-                "#...##.#.#".to_string(),
-                "#.#.#.#...".to_string(),
-                "...#..###.".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&2729),
-            Some(&[
-                "...#.#.#.#".to_string(),
-                "#..#......".to_string(),
-                ".##...##.#".to_string(),
-                "####....#.".to_string()
-            ])
-        );
-        assert_eq!(
-            edge_map.get(&3079),
-            Some(&[
-                "#.#.#####.".to_string(),
-                ".#....#...".to_string(),
-                "...###.#..".to_string(),
-                "...#.##..#".to_string()
-            ])
-        );
-    }
+    //     #[test]
+    //     fn test_parse_input() {
+    //         let edge_map = parse_input(TEST_INPUT).unwrap().1;
+    //         assert_eq!(
+    //             edge_map.get(&2311),
+    //             Some(&[
+    //                 "..##.#..#.".to_string(),
+    //                 "...#.##..#".to_string(),
+    //                 "###..###..".to_string(),
+    //                 ".#..#####.".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&1951),
+    //             Some(&[
+    //                 "#.##...##.".to_string(),
+    //                 ".#####..#.".to_string(),
+    //                 "..#.##...#".to_string(),
+    //                 "#..#..#.##".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&1171),
+    //             Some(&[
+    //                 "####...##.".to_string(),
+    //                 ".#..#.....".to_string(),
+    //                 "...##.....".to_string(),
+    //                 ".##....###".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&1427),
+    //             Some(&[
+    //                 "###.##.#..".to_string(),
+    //                 "..###.#.#.".to_string(),
+    //                 ".#..#.##..".to_string(),
+    //                 "......#..#".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&1489),
+    //             Some(&[
+    //                 "##.#.#....".to_string(),
+    //                 ".....#..#.".to_string(),
+    //                 "..#.##.###".to_string(),
+    //                 "#.#.##...#".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&2473),
+    //             Some(&[
+    //                 "#....####.".to_string(),
+    //                 "...###.#..".to_string(),
+    //                 ".#.#.###..".to_string(),
+    //                 ".##...####".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&2971),
+    //             Some(&[
+    //                 "..#.#....#".to_string(),
+    //                 "#...##.#.#".to_string(),
+    //                 "#.#.#.#...".to_string(),
+    //                 "...#..###.".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&2729),
+    //             Some(&[
+    //                 "...#.#.#.#".to_string(),
+    //                 "#..#......".to_string(),
+    //                 ".##...##.#".to_string(),
+    //                 "####....#.".to_string()
+    //             ])
+    //         );
+    //         assert_eq!(
+    //             edge_map.get(&3079),
+    //             Some(&[
+    //                 "#.#.#####.".to_string(),
+    //                 ".#....#...".to_string(),
+    //                 "...###.#..".to_string(),
+    //                 "...#.##..#".to_string()
+    //             ])
+    //         );
+    //     }
 
-    #[test]
-    fn test_find_neighbors() {
-        let tiles = parse_input(TEST_INPUT).unwrap().1;
-        let neighbor_map = find_neighbors(&tiles);
+    //     #[test]
+    //     fn test_find_neighbors() {
+    //         let tiles = parse_input(TEST_INPUT).unwrap().1;
+    //         let neighbor_map = find_neighbors(&tiles);
 
-        assert_eq!(
-            neighbor_map.get(&1951),
-            Some(&[Some((2729, 2, false)), Some((2311, 3, false)), None, None])
-        );
-        assert_eq!(
-            neighbor_map.get(&1427),
-            Some(&[
-                Some((1489, 2, false)),
-                Some((2473, 2, false)),
-                Some((2311, 0, false)),
-                Some((2729, 1, false))
-            ])
-        );
-    }
+    //         assert_eq!(
+    //             neighbor_map.get(&1951),
+    //             Some(&[Some((2729, 2, false)), Some((2311, 3, false)), None, None])
+    //         );
+    //         assert_eq!(
+    //             neighbor_map.get(&1427),
+    //             Some(&[
+    //                 Some((1489, 2, false)),
+    //                 Some((2473, 2, false)),
+    //                 Some((2311, 0, false)),
+    //                 Some((2729, 1, false))
+    //             ])
+    //         );
+    //     }
 
     #[test]
     fn test_part1() {
-        let edges = parse_input(TEST_INPUT).unwrap().1;
-        let neighbor_map = find_neighbors(&edges);
+        let tiles = parse_input(TEST_INPUT).unwrap();
+        let neighbor_map = find_neighbors(&tiles);
         assert_eq!(part1(&neighbor_map), 20899048083289);
-    }
-
-    #[test]
-    fn test_part2() {
-        let (tile_map, edge_map) = parse_input(TEST_INPUT).unwrap();
-        let neighbor_map = find_neighbors(&edge_map);
-        assert_eq!(part2(&neighbor_map, &tile_map), 273);
     }
 }
